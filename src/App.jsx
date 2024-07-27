15baa1f4da55ba8e00ed4e6299e5b93f9eb77442
Jack Cahit Karakas <ckarakassdet@gmail.com>
2024-07-26 23:14:43 -0500

    for newer react project, we do not need the Fragment even

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, Fragment } from 'react';
+import { useState } from 'react';
 
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
@@ -32,7 +32,7 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -74,7 +74,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </Fragment>
+    </>
   );
 }
 
