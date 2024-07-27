c404b279dd6507f936b5b65429cd5c4431a29f96
Jack Cahit Karakas <ckarakassdet@gmail.com>
2024-07-26 23:12:37 -0500

    use Fragment instead of adding div

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
@@ -5,3 +6,5 @@ import "./index.css";
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
+// The following code can be used to run for without using react. 
+// ReactDOM.createRoot(entryPoint).render(React.createElement(App)); 
