1842ba8257b1bd589cd8ad86f9dbe960a73605d9
Jack Cahit Karakas <ckarakassdet@gmail.com>
2024-07-28 00:46:26 -0500

    manually managing props, not preferred

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { EXAMPLES } from "../data.js";
 
 import TabButton from "../components/TabButton.jsx";
+import Section from "./Section.jsx";
 
 export default function Examples() {
 
@@ -28,8 +29,7 @@ export default function Examples() {
     }
 
     return (
-        <section id="examples">
-            <h2>Examples</h2>
+        <Section title="Examples" id="examples">
             <menu>
                 <TabButton
                     isSelected={selectedTopic === 'components'}
@@ -57,6 +57,6 @@ export default function Examples() {
                 </TabButton>
             </menu>
             {tabContent}
-        </section>
+        </Section>
     );
 }
